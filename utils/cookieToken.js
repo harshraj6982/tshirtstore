ccf17151f959ae09103c2c97e36b73da229e0e3f
harshraj6982 <41638655+harshraj6982@users.noreply.github.com>
2022-01-30 00:41:07 +0530

    Fomatted cookieToken.js

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -1,21 +1,21 @@
 const cookieToken = (user, res) => {
-    const token = user.getJwtToken();
+  const token = user.getJwtToken();
   
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-    };
-  
-    user.password = undefined;
-    res.status(200).cookie("token", token, options).json({
-      success: true,
-      token,
-      user,
-    });
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
   };
   
-  module.exports = cookieToken;
+  user.password = undefined;
+  res.status(200).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+  
+module.exports = cookieToken;
   
